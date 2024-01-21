636f6bf15770f9f0097ce9a7b50c0b82fd6da0b5
Jakub Matuszczak <kubamatuszczak2@gmail.com>
2024-01-21 18:35:48 +0100

    Co-authored-by: Olsza <J0lszewskii@users.noreply.github.com>

--- README.md
+++ README.md
@@ -14,7 +14,30 @@
 # 3. long term
 - skrypt llm czytający newsy i analizuje je czy jest to dobry news dla danej akcji czy zły
 - dodawnie wartości newsom
-- dodać skale od -10 do 10 i bard będzie analizował jaką wartość przyznać newsom 
+- dodać skale od -10 do 10 i bard będzie analizował jaką wartość przyznać newsom
+
+
+
+
+# 1 Analiza wskaźnikó
+- Rsi - mierzy prędkość i zmiany cen, pomagając inwestorom zidentyfikować, czy aktywo jest przekupione (nadkupione) lub przesprzedane (nadzwyczaj sprzedawane).
+- Przekupienie rynku - Stan, w którym cena danego aktywa jest uznawana za wysoką w stosunku do swojej przewidywanej wartości
+- Przesprzedarz - Stan, w którym cena danego aktywa jest uznawana za niską w stosunku do swojej przewidywanej wartości.
+
+- ATR - TR pomaga określić zmienność rynku, 
+
+- NATR - jest to znormalizowana wersja ATR, co pozwala porównać zmienność między różnymi aktywami.
+
+- AVGPRICE - oblicza średnią wartość cenową na podstawie  Dostarcza informacje na temat przeciętnej ceny danego okresu.
+
+- ADX -  opis: ADX określa siłę trendu, a nie jego kierunek. Wartości powyżej 25 wskazują na silny trend.
+Zastosowanie: Pomaga inwestorom ocenić, czy na rynku występuje trend oraz jego siłę.
+
+- MACD -Opis: MACD to wskaźnik momentum, który porównuje dwie średnie kroczące dla cen.
+Zastosowanie: Pomaga inwestorom zidentyfikować potencjalne punkty zwrotne na rynku.
+
+- SMA - Opis: SMA to średnia krocząca prostą, która wygładza cenę, pomagając identyfikować ogólny trend.
+Zastosowanie: Pomaga inwestorom zobaczyć ogólny kierunek ruchu cenowego.
 
 
 
--- main.py
+++ main.py
@@ -25,30 +25,32 @@ def truncate(n, decimals=0):
 def get_stock(stock):
     data = yfinance.download(tickers=ticker, period='30d', interval='1h')
     return data
-
+ticker='AAPL'
 #wyświetla makapaka
 print("makapaka")
 #sidebar po lewej stronie strony
-with st.sidebar.form(key='my_form'):
-    #utworzenie dwóch kolumn w sidebarze
-    col1, col2 = st.columns(2)
-    #wyświetla wartości z biblioteki yfinance w dwóch kolumnach w sidebarze
-    with col1:
-        st.metric(label=f":green[Zamknięcie ] :green[{st.session_state['current_ticker']}]", value=f"{truncate(get_stock(st.session_state['current_ticker'])['Close'][0], 3)}$", delta=f"{truncate((get_stock(st.session_state['current_ticker'])['Close'][1] - get_stock(st.session_state['current_ticker'])['Close'][0])*-1, 2)}$")
-    with col2:
-        st.metric(label=f":green[Otwarcie ] :green[{st.session_state['current_ticker']}]", value=f"{truncate(get_stock(st.session_state['current_ticker'])['Open'][0], 3)}$", delta=f"{truncate((get_stock(st.session_state['current_ticker'])['Open'][1] - get_stock(st.session_state['current_ticker'])['Open'][0])*-1, 2)}$")
-   #make the descriptions short
-    ticker = st.text_input('Podaj symbol akcji', 'AAPL')
-    st.session_state['current_ticker'] = ticker
-    doRSI = st.checkbox('RSI - Przekupienie i przesprzedanie')
-    doATR = st.checkbox('ATR - Im wyżysza wartość, tym większa zmienność')
-    doNATR = st.checkbox('NATR - Im wyżysza wartość, tym większa zmienność')
-    doAVGPRICE = st.checkbox('AVGPRICE - Oblicza średnią wartość cenową (dostarcza informacje na temat przeciętnej ceny) ')
-    doADX = st.checkbox('ADX - Podsumowując, ADX pomaga inwestorom określić, czy rynek znajduje się w fazie trendu i oceniać jego siłę')
-    doMACD = st.checkbox('MACD - dostarcza informacji o dynamice zmian w trendzie. ')
-    doSMA = st.checkbox('SMA - indentyfikuje ogólny kierunek ruchu cenowego ')
-    doBollingerBands = st.checkbox('Bollinger Bands - Przekupienie i przesprzedanie, odwracanie ceny')
-    submit_button = st.form_submit_button(label='Execute')
+with st.sidebar:
+    with st.form(key='my_form'):
+        #utworzenie dwóch kolumn w sidebarze
+        col1, col2 = st.columns(2)
+        #wyświetla wartości z biblioteki yfinance w dwóch kolumnach w sidebarze
+        with col1:
+            st.metric(label=f":green[Zamknięcie ] :green[{st.session_state['current_ticker']}]", value=f"{truncate(get_stock(st.session_state['current_ticker'])['Close'][0], 3)}$", delta=f"{truncate((get_stock(st.session_state['current_ticker'])['Close'][1] - get_stock(st.session_state['current_ticker'])['Close'][0])*-1, 2)}$")
+        with col2:
+            st.metric(label=f":green[Otwarcie ] :green[{st.session_state['current_ticker']}]", value=f"{truncate(get_stock(st.session_state['current_ticker'])['Open'][0], 3)}$", delta=f"{truncate((get_stock(st.session_state['current_ticker'])['Open'][1] - get_stock(st.session_state['current_ticker'])['Open'][0])*-1, 2)}$")
+        #make the descriptions short
+        ticker = st.text_input('Podaj symbol akcji', 'AAPL')
+        st.session_state['current_ticker'] = ticker
+        doRSI = st.checkbox('RSI - Przekupienie i przesprzedanie')
+        doATR = st.checkbox('ATR - Im wyżysza wartość, tym większa zmienność')
+        doNATR = st.checkbox('NATR - Im wyżysza wartość, tym większa zmienność')
+        doAVGPRICE = st.checkbox('AVGPRICE - Oblicza średnią wartość cenową (dostarcza informacje na temat przeciętnej ceny) ')
+        doADX = st.checkbox('ADX - Podsumowując, ADX pomaga inwestorom określić, czy rynek znajduje się w fazie trendu i oceniać jego siłę')
+        doMACD = st.checkbox('MACD - dostarcza informacji o dynamice zmian w trendzie. ')
+        doSMA = st.checkbox('SMA - indentyfikuje ogólny kierunek ruchu cenowego ')
+        doBollingerBands = st.checkbox('Bollinger Bands - Przekupienie i przesprzedanie, odwracanie ceny')
+        submit_button = st.form_submit_button(label='Execute')
+    st.empty()
     #sprawdza czy przycisk został wciśnięty
     if submit_button:
         st.success('Wykonano!', icon='👏')
@@ -89,17 +91,22 @@ data['ATR'] = data['ATR'].replace(np.nan, 0)
 data['NATR'] = data['NATR'].replace(np.nan, 0)
 data['AVGPRICE'] = data['AVGPRICE'].replace(np.nan, 0)
 data['ADX'] = ta.ADX(data['High'], data['Low'], data['Close'], timeperiod=14)
-data['MACD'], _, _ = ta.MACD(data['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
-data['SMA'] = ta.SMA(data['Close'], timeperiod=14)
+data['MACD'], data['MACD_signal'], _ = ta.MACD(data['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+data['SMA_short'] = ta.SMA(data['Close'], timeperiod=14)
+data['SMA_long'] = ta.SMA(data['Close'], timeperiod=28)
 upper_band, middle_band, lower_band = ta.BBANDS(data['Close'], timeperiod=10, nbdevup=2, nbdevdn=2)
 data['UpperBand'] = upper_band
 data['MiddleBand'] = middle_band
 data['LowerBand'] = lower_band
 
+
+
 #Wyświetlanie wskaźników i ich opisów z TA-lib w zaleźniści, które zaznaczono w sidebarze
 if doRSI:
     st.header("RSI")
-    st.subheader("Przekupienie i przesprzedanie")
+    st.subheader("Mierzy prędkość i zmiany cen, pomagając inwestorom zidentyfikować, czy aktywo jest :blue[przekupione (nadkupione) lub przesprzedane (nadzwyczaj sprzedawane] :red[Przekupienie rynku] - Stan, w którym cena danego aktywa jest uznawana za :red[wysoką]w stosunku do swojej red:[przewidywanej wartości]  :green[Przesprzedarz] - Stan, w którym cena danego aktywa jest uznawana za :green[niską] w stosunku do swojej :green[przewidywanej wartości])")
+    st.write(" RSI - powyżej 70 przekupienie, poniżej 30 przesprzedanie, pomiędzy 30 a 70 neutralnie, jeśli jest przekupione i spada, to może być sygnał do sprzedaży, jeśli jest przesprzedane i rośnie, to może być sygnał do kupna")
+
     st.line_chart(data['RSI'])
 if doATR:
     st.header("ATR")
@@ -144,7 +151,24 @@ if doMACD:
 if doSMA:
     st.header("SMA")
     st.subheader("SMA jest używane do identyfikowania :green[ogólnego trendu cenowego]. Kiedy aktualna cena :green[przekracza SMA], może to sugerować :green[wzrost cen], a gdy cena :red[spada poniżej SMA], może to sugerować :red[spadek cen].) ")
-    st.line_chart(data['SMA'])
+    #linechart in go for sma short and long
+
+    # sort data by date
+    data = data.sort_index(ascending=True, axis=0)
+
+    
+
+    # slider for how many data to show
+    ile_danych = st.slider('Ile danych pokazać?', 1, 200, 12)
+    ile_danych = abs(ile_danych - 200)
+    # add line for sma long to linechart above
+    smas = go.Figure(data=[go.Scatter(x=data.index[ile_danych:], y=data['SMA_short'][ile_danych:], name='SMA_short'),])
+    smas.add_trace(go.Scatter(x=data.index[ile_danych:], y=data['SMA_long'][ile_danych:], name='SMA_long'))
+    smas.add_trace(go.Scatter(x=data.index[ile_danych:], y=data['Close'][ile_danych:], name='Close'))
+    smas.update_layout(title='SMA', xaxis_title='Data', yaxis_title='Cena', template='plotly_dark')
+    st.plotly_chart(smas)
+    #linechart in st for sma short and long
+    
 if doBollingerBands:
         st.header("Bollinger Bands")
         if st.checkbox("Rozprawka 🤓"): st.subheader("Pomaga inwestorom ocenić, czy aktywo jest :green[przekupione] czy :red[przesprzedane], a także określić potencjalne poziomy wsparcia i oporu W praktyce, kiedy ceny zbliżają się do górnego pasa, może to sugerować, że aktywo jest przekupione, a kiedy zbliżają się do dolnego pasa, może to sugerować, że jest przesprzedane. Inwestorzy szukają sygnałów odwrócenia trendu lub potencjalnych punktów wejścia lub wyjścia na rynku w oparciu o relację cen do pasm.")
@@ -166,4 +190,51 @@ if doBollingerBands:
         # Wyświetlenie wykresu
         st.plotly_chart(fig)
 
-    
+
+
+
+#Sprawdzanie czy kolorowaćazwy wskaźników
+#ATR
+#oblicznie średniej atr
+średniaATR = data['ATR'].mean()
+
+
+
+
+
+# Dotychczas opisane wskaźniki to: RSI, ATR, NATR, AVGPRICE, ADX, MACD, SMA, Bollinger Bands
+# Relative Strength Index (RSI):
+# Overbought: Typically above 70
+# Oversold: Typically below 30
+# Average True Range (ATR):
+# No specific rise or fall thresholds. ATR measures volatility, so larger values indicate higher volatility, and smaller values indicate lower volatility.
+# Normalized Average True Range (NATR):
+# NATR is a normalized version of ATR, so there isn't a standard threshold for rise or fall. It reflects the volatility as a percentage.
+# Average Price (AVGPRICE):
+# AVGPRICE is the average of the high, low, and close prices. No specific rise or fall thresholds are commonly used.
+# Average Directional Index (ADX):
+# Generally, an ADX value above 25 may indicate a trending market. A rising ADX suggests a strengthening trend, while a falling ADX suggests a weakening trend.
+# Moving Average Convergence Divergence (MACD):
+# Signal Line Crossovers: Bullish signal when MACD crosses above the signal line. Bearish signal when MACD crosses below the signal line.
+# Histogram: Bullish when histogram bars rise above the zero line. Bearish when bars fall below the zero line.
+# Simple Moving Average (SMA):
+# SMA crossovers: Bullish when a shorter-term SMA crosses above a longer-term SMA. Bearish when a shorter-term SMA crosses below a longer-term SMA.
+# Bollinger Bands:
+# Overbought: When the price touches or exceeds the upper band.
+# Oversold: When the price touches or falls below the lower band.
+
+
+
+
+
+
+# RSI - powyżej 70 przekupienie, poniżej 30 przesprzedanie, pomiędzy 30 a 70 neutralnie, jeśli jest przekupione i spada, to może być sygnał do sprzedaży, jeśli jest przesprzedane i rośnie, to może być sygnał do kupna
+# ATR - Im wyżysza wartość, tym większa zmienność, jest to różnica między najwyższą a najniższą ceną
+# NATR - Im wyżysza wartość, tym większa zmienność, jest to różnica między najwyższą a najniższą ceną, od ATR różni się tym, że jest to wartość procentowa
+# AVGPRICE - Oblicza średnią wartość cenową (dostarcza informacje na temat przeciętnej ceny) 
+# ADX - powyżej 25 silny trend, poniżej 20 słaby lub brak trendu
+# MACD - prognozowany spadek kiedy linia sygnałowa będzie powyżej linii MACD, prognozowany wzrost kiedy linia sygnałowa będzie poniżej linii MACD
+# SMA - 
+# Cena powyżej SMA: Może sugerować wzrost cen.
+# Cena poniżej SMA: Może sugerować spadek cen.        
+
