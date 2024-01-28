69bb41df06cab53986fcc5d5ed2d46d987b9d552
Jakub Matuszczak <kubamatuszczak2@gmail.com>
2024-01-28 19:14:24 +0100

    Co-authored-by: Olsza <J0lszewskii@users.noreply.github.com>

--- świeczuszki.py
+++ świeczuszki.py
@@ -9,21 +9,19 @@ def Świeczuszki(świeczuszki, data):
     świeczki['Doji'] = ta.CDLDOJI(data['Open'], data['High'], data['Low'], data['Close'])
     świeczki['Doji Star'] = ta.CDLDOJISTAR(data['Open'], data['High'], data['Low'], data['Close'])
     świeczki['Dragonfly Doji'] = ta.CDLDRAGONFLYDOJI(data['Open'], data['High'], data['Low'], data['Close'])
+    świeczki['Two Crows'] = ta.CDL2CROWS(data['Open'], data['High'], data['Low'], data['Close'])
+    świeczki['Three Black Crows'] = ta.CDL3BLACKCROWS(data['Open'], data['High'], data['Low'], data['Close'])
+    świeczki['Three Inside Up/Down'] = ta.CDL3INSIDE(data['Open'], data['High'], data['Low'], data['Close'])  # Added this line
     print(świeczki)
-
-    
 
     lista = []
 
-    świeczuszki.write(świeczki)
-    # wyświetlanie świeczek które są
+    st.table(świeczki)
     st.sidebar.subheader("Świeczki które są: ")
-    # jeśli w ostatnich 4 datapointach jest doji to wyświetl doji w kolorze zielonym i objaśnij co to jest
     for i in świeczki.columns:
         for y in świeczki[i][-4:]:
             if y == 100:
                 lista.append(i)
-
 
     if 'Doji' in lista:
         st.sidebar.write(":green[Doji]")
@@ -31,6 +29,11 @@ def Świeczuszki(świeczuszki, data):
         st.sidebar.write(":green[Doji Star]")
     if 'Dragonfly Doji' in lista:
         st.sidebar.write(":green[Dragonfly Doji]")
+    if 'Two Crows' in lista:
+        st.sidebar.write(":green[Two Crows]")
+    if 'Three Black Crows' in lista:
+        st.sidebar.write(":green[Three Black Crows]")
+    if 'Three Inside Up/Down' in lista:  # Added this condition
+        st.sidebar.write(":green[Three Inside Up/Down]")  # Added this line
     return świeczki
-
 
